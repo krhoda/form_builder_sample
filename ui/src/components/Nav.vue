5e24cd53b9a0e479672876bd625163807a464e3a
K Rhoda <kelseydrhoda@gmail.com>
2020-11-07 20:01:29 -0700

    initial commit, the canonical message

---
+++ ui/src/components/Nav.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="hello">
+      <a :href="`${host}/form-builder`" >Form Builder</a>
+      <a :href="`${host}/form-finder`" >Form Finder</a>
+      <a :href="`${host}/sumbission-finder`" >Submission Builder</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  data: () => {
+    return {
+      host: window.location.host,
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
